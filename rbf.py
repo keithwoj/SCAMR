062dcc0e508f7ce9b4cc438b712679372b52aa45
Keith Wojciechowski <wojciechowskik@uwstout.edu>
2015-10-17 23:55:40 -0500

    Added comments and ran unit tests

--- rbf.py
+++ rbf.py
@@ -10,10 +10,32 @@ radial basis function (RBF) interpolation and differentiation.
 dmatrix constructs the distance matrix between points and centers
 
 rbfinterp solves the collocation problem (interpolate) data to a surface (curve)
-*** rbfinterp currently uses the distance matrix to fit the data to the surface
-*** it needs an rbf function
-*** TO DO!!!
 
+RBFs:
+dmatrix is the linear RBF, f(r) = r
+dmatrix(d,c)
+
+cubic, f(r) = r^3
+cubic(d,c,order=0,ep=0)
+
+multiquadric, f(r) = sqrt(1 + (ep r)^2)
+mq(d,c,order=0,ep=1)
+
+gauss, f(r) = exp(-(ep r)^2)
+gauss(d,c,order=0,ep=1)
+
+RBF Remarks:
+(1) r is the distance matrix, dmatrix(d,c) where d = data, c = centers
+(2) ep is the shape parameter epsilon
+(3) the interface for each RBF function is (d,c,order,ep)
+    d = data, c = centers, order = derivative order, ep = shape parameter
+(4) Even though an RBF does not require a shape parameter, send one anyway, this
+    convention permits a general differentiation matrix interface for RBF-FD
+(5) RBFs are globally supported but not all are strictly positive definite
+
+*** TO DO: CONSTRUCT RBF-GLOBAL DIFFERENTIATION MATRICES
+*** TO DO: CONSTRUCT RBF-FD DIFFERENTIATION MATRICES
+*** TO DO: CONSTRUCT RBF-LA DIFFERENTIATION MATRICES
 
 *** Unit tests for each function are contained in the module ***
 """
@@ -96,27 +118,41 @@ def dmatrix(d,c):
     # Finish distance formula by taking square root of each entry
     return sqrt(DM)
 
-def rbfinterp(d,c,s,ep):
+def rbfinterp(d,c,s,p,func = 'dist',ep=1):
     #
     # *** TO DO: create an rbf function, allow user input for rbf type
     #
-    # yp = rbfinterp(d,c,s,ep)
+    # yp = rbfinterp(d,c,s,p)
     #
     # Use Radial Basis Functions (rbf) to interpolate
     #
     # Arguments:
     # d = data, c = centers
     # s = surface (curve) to be interpolated
-    # ep = evaluation points (points at which s is to be interpolated)
+    # p = evaluation points (points at which s is to be interpolated)
+    # rbf = Radial Basis Function (RBF) to be used for interpolation
+    #       default = Euclidian distance matrix
     #
-    # Construct distance matrix for the data and centers
-    IM = dmatrix(d,c)
-    # Construct distance matrix for the evaluation points and centers
-    EM = dmatrix(ep,c)
-    #
+    # Construct the rbf
+    # *** TO DO: convert to a dictionary or hashmap (with derivatives, etc.)
+    if func == 'dist':
+        IM = dmatrix(d,c)
+        EM = dmatrix(p,c)
+    elif func == 'mq':
+        IM = mq(d,c,0,ep)
+        EM = mq(p,c,0,ep)
+    elif func == 'cubic':
+        IM = cubic(d,c,0,ep)
+        EM = cubic(p,c,0,ep)
+    elif func == 'gauss':
+        IM = gauss(d,c,0,ep)
+        EM = gauss(p,c,0,ep)
+    else:
+        raise NameError("Enter a valid RBF.")    
+    #***************************************************************************
     # Linear Algebra Remarks:
     # 
-    # IM*a = s is a system of equations where the coefficients, a, are unknown
+    # P*a = s is a system of equations where the coefficients, a, are unknown
     # This matrix system is called the "collocation problem," i.e. What weights
     # are needed so that a linear combination of basis functions and weights
     # yeilds a point on the surface?
@@ -124,13 +160,35 @@ def rbfinterp(d,c,s,ep):
     # interpolant.
     #
     # Summary:
-    # IM*a = s => a = inv(IM)*s
+    # P*a = s => a = inv(P)*s
     # EM*a = yp where yp is the interpolant and EM is the matrix with entries
     # that are known basis functions at the evaluation points.
     #
-    # Since a = inv(IM)*s, EM*a = yp => EM*inv(IM)*s
+    # Since a = inv(P)*s, EM*a = yp => EM*inv(P)*s
     #
     return dot(EM,linalg.solve(IM,s))
+
+'''
+********************************************************************************
+                                    RBF ZOO
+********************************************************************************
+'''
+def cubic(d,c,order=0,ep=0):
+    # cubic, f(r) = r^3
+    DM = dmatrix(d,c)
+    return DM**3
+    
+def mq(d,c,order=0,ep=1):
+    # multiquadric, f(r) = sqrt(1 + (ep r)^2)
+    DM = dmatrix(d,c)
+    e = ep*ones(DM.shape)
+    return sqrt(1+(e*DM)**2)
+
+def gauss(d,c,order=0,ep=1):
+    # gaussian, f(r) = exp(-(ep r)^2)
+    DM = dmatrix(d,c)
+    e = ep*ones(DM.shape)
+    return exp(-(e*DM)**2)
 
 '''
 ********************************************************************************
@@ -140,12 +198,57 @@ def rbfinterp(d,c,s,ep):
 def testfunction(data):
     # N-D Gaussian
     N, sd = data.shape
-    p = ones((N,1))
+    f = ones((N,1))
     for i in xrange(sd):
-        p = p*array([exp(-15*(data[:,i]-0.5)**2)]).T
+        f = f*array([exp(-15*(data[:,i]-0.5)**2)]).T
         
-    return p
+    return f
+    
+def test_interp():
+    # Testing interpolation
+    x = linspace(0,1,19)    
+    xp = linspace(0.01,0.99,33)
+    rbf_list = ['dist','cubic','mq','gauss']
+    ep_list = [0.01,0.5,1.0,1.5,2]
+    for ep in ep_list:
+        for ff in rbf_list:
+            # 1D
+            data = array([x]).T
+            ctrs = data
+            p = array([xp]).T
+            rhs = testfunction(data)
+            exact = testfunction(p)
+            Pf = rbfinterp(data,ctrs,rhs,p,ff,ep)
 
+            err = norm(Pf-exact)
+        
+            print "1D interp, %s, shape = %f, L2 error = %e " % (ff,ep,err)
+    
+            # 2D
+            data = array([x,x]).T
+            ctrs = data
+            p = array([xp,xp]).T
+            rhs = testfunction(data)
+            exact = testfunction(p)
+            Pf = rbfinterp(data,ctrs,rhs,p,ff,ep)
+        
+            err = norm(Pf-exact)
+        
+            print "2D interp, %s, shape = %f, L2 error = %e " % (ff,ep,err)
+        
+            # 3D
+            data = array([x,x,x]).T
+            ctrs = data
+            p = array([xp,xp,xp]).T
+            rhs = testfunction(data)
+            exact = testfunction(p)
+            Pf = rbfinterp(data,ctrs,rhs,p,ff,ep)
+        
+            err = norm(Pf-exact)
+        
+            print "3D interp, %s, shape = %f, L2 error = %e " % (ff,ep,err) 
+            print "----------------------------------------------------"
+            
 def test_dmatrix():
     # Unit tests for the dmatrix function
     x = linspace(0,1,5)
@@ -190,44 +293,3 @@ def test_dmatrix():
     ctrs = data
     DM = dmatrix(data,ctrs)
     print DM
-    
-def test_interp():
-    # Testing interpolation
-    x = linspace(0,1,19)    
-    xp = linspace(0.01,0.99,33)
-    
-    # 1D
-    data = array([x]).T
-    ctrs = data
-    ep = array([xp]).T
-    rhs = testfunction(data)
-    exact = testfunction(ep)
-    Pf = rbfinterp(data,ctrs,rhs,ep)
-    
-    err = str(norm(Pf-exact))
-    
-    print "L2 error for 1D interpolation is " + err
-
-    # 2D
-    data = array([x,x]).T
-    ctrs = data
-    ep = array([xp,xp]).T
-    rhs = testfunction(data)
-    exact = testfunction(ep)
-    Pf = rbfinterp(data,ctrs,rhs,ep)
-    
-    err = str(norm(Pf-exact))
-    
-    print "L2 error for 2D interpolation is " + err
-    
-    # 3D
-    data = array([x,x,x]).T
-    ctrs = data
-    ep = array([xp,xp,xp]).T
-    rhs = testfunction(data)
-    exact = testfunction(ep)
-    Pf = rbfinterp(data,ctrs,rhs,ep)
-    
-    err = str(norm(Pf-exact))
-    
-    print "L2 error for 3D interpolation is " + err   
